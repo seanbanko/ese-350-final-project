55c54551cc826cbf0ce731b19732cba1ab0b9bef
Sean Banko <sean.banko@gmail.com>
2022-04-17 14:35:53 -0400

    Attempt burst read and add debugging to while loops

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@
 #define WHO_AM_I 0x75
 
 char str[50];
-int data;
+int data[6];
 
 float acc_x;
 float acc_y;
@@ -36,99 +36,104 @@ void single_read_sequence();
 void clear_sleep_bit();
 
 int main(void) {
-
   serialInit(BAUD_PRESCALER);
   twi_init();
 
-  sprintf(str, "after twi init\n");
+  sprintf(str, "initialization complete\n");
   serialPrint(str);
 
   clear_sleep_bit();
-  sprintf(str, "after write sequence\n");
+
+  sprintf(str, "cleared sleep bit\n");
   serialPrint(str);
 
   while (1) {
     single_read_sequence();
-
-    // float roll = atan2(acc_y, acc_z) * (180 / M_PI);
-    // float pitch =
-    //     atan2(acc_x, sqrt(pow(acc_y, 2) + pow(acc_z, 2))) * (180 / M_PI);
-    // if (pitch > PITCH_THRESHOLD) {
-    // }
-
+    for (int i = 0; i < 6; i++) {
+      sprintf(str, "data[%d]: %08d\n", i, data[i]);
+      serialPrint(str);
+    }
     _delay_ms(500);
   }
 }
 
 void clear_sleep_bit() {
   twi_start();
-  _delay_ms(5);
   // Send device address and write bit (SLA+W) and wait for ACK
   TWDR = (MPU6050_ADDR << 1) | 0;
   TWCR = (1 << TWINT) | (1 << TWEN);
-  while ((TWSR & 0xF8) != TWI_T_ADDR_ACK)
-    ;
-  _delay_ms(5);
+  while ((TWSR & 0xF8) != TWI_T_ADDR_ACK) {
+    sprintf(str, "waiting after SLA+W\n");
+    serialPrint(str);
+    sprintf(str, "TWSR: %x\n", TWSR);
+    serialPrint(str);
+    _delay_ms(500);
+  }
   // Send register address (RA) and wait for ACK
   TWDR = PWR_MGMT_1;
   TWCR = (1 << TWINT) | (1 << TWEN);
-  while ((TWSR & 0xF8) != TWI_T_DATA_ACK)
-    ;
-  _delay_ms(5);
+  while ((TWSR & 0xF8) != TWI_T_DATA_ACK) {
+    sprintf(str, "waiting after RA\n");
+    serialPrint(str);
+    sprintf(str, "TWSR: %x\n", TWSR);
+    serialPrint(str);
+    _delay_ms(500);
+  }
   // Send DATA and wait for ACK
   TWDR = 0x00;
   TWCR = (1 << TWINT) | (1 << TWEN);
-  while ((TWSR & 0xF8) != TWI_T_DATA_ACK)
-    ;
-  _delay_ms(5);
+  while ((TWSR & 0xF8) != TWI_T_DATA_ACK) {
+    sprintf(str, "waiting after DATA\n");
+    serialPrint(str);
+    sprintf(str, "TWSR: %x\n", TWSR);
+    serialPrint(str);
+    _delay_ms(500);
+  }
   twi_stop();
   _delay_ms(5);
 }
 
 void single_read_sequence() {
   twi_start();
-  _delay_ms(5);
   // Send device address and write bit (SLA+W) and wait for ACK
   TWDR = (MPU6050_ADDR << 1) | 0;
   TWCR = (1 << TWINT) | (1 << TWEN);
-  while ((TWSR & 0xF8) != TWI_T_ADDR_ACK)
-    ;
-  _delay_ms(5);
+  while ((TWSR & 0xF8) != TWI_T_ADDR_ACK) {
+    sprintf(str, "waiting after SLA+W\n");
+    serialPrint(str);
+    sprintf(str, "TWSR: %x\n", TWSR);
+    serialPrint(str);
+    _delay_ms(500);
+  }
   // Send register address (RA) and wait for ACK
   TWDR = ACCEL_XOUT_H;
   TWCR = (1 << TWINT) | (1 << TWEN);
   while ((TWSR & 0xF8) != TWI_T_DATA_ACK)
     ;
-  _delay_ms(5);
   // Send repeated start (RS) and wait for ACK
   TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
   while ((TWSR & 0xF8) != TWI_RSTART)
     ;
-  _delay_ms(5);
   // Send device address and read bit (SLA+R) and wait for ACK + DATA
   TWDR = (MPU6050_ADDR << 1) | 1;
   TWCR = (1 << TWINT) | (1 << TWEN);
   while ((TWSR & 0xF8) != TWI_R_ADDR_ACK)
     ;
-  _delay_ms(5);
+  int i;
+  for (i = 0; i < 5; i++) {
+    // Send ACK
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+    while ((TWSR & 0xF8) != TWI_R_DATA_NACK)
+      ;
+    // Store DATA
+    data[i] = TWDR;
+  }
   // Send NACK
   TWCR = (1 << TWINT) | (1 << TWEN);
   while ((TWSR & 0xF8) != TWI_R_DATA_NACK)
     ;
-  _delay_ms(5);
   // Store DATA
-  data = TWDR;
+  data[i] = TWDR;
 
   twi_stop();
-
-  sprintf(str, "raw data: %08d\n", data);
-  serialPrint(str);
-
-  sprintf(str, "data << 8: %08d\n", (data << 8));
-  serialPrint(str);
-
-  acc_x = (data << 8) / 16384.0;
-
-  sprintf(str, "acc_x: %f\n", acc_x);
-  serialPrint(str);
 }
--- src/twi/twi.c
+++ src/twi/twi.c
@@ -5,11 +5,14 @@
 #include <util/delay.h>
 
 #include "twi.h"
+#include "../serial/serial.h"
+
+char str[30];
 
 // Initialize TWI
 void twi_init() {
   // Enable internal pull-up resistors for SCL and SDA
-  PORTC &= (1 << PORTC4) | (1 << PORTC5);
+  // PORTC &= (1 << PORTC4) | (1 << PORTC5);
   // TODO dont make this a magic number
   // SCL_freq = 16MHz/(16 + 2*12*1) = 400KHz	*/
   TWBR = 12;
@@ -18,12 +21,13 @@ void twi_init() {
 // Send start (S) and wait for correct status
 void twi_start() {
   TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-  while ((TWSR & 0xF8) != TWI_START)
-    ;
+  while ((TWSR & 0xF8) != TWI_START) {
+    sprintf(str, "waiting after START\n");
+    serialPrint(str);
+    sprintf(str, "TWSR: %x\n", TWSR);
+    serialPrint(str);
+  }
 }
 
 // Send stop (P)
-void twi_stop() {
-  TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
-}
-
+void twi_stop() { TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN); }
