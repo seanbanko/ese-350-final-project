ea7a868a1fff87046f4b34ef6ae4b4ad74e71a2c
Sean Banko <sean.banko@gmail.com>
2022-04-20 17:27:32 -0400

    Fix: uploaded wrong NodeMCU code

--- nodemcu.ino
+++ nodemcu.ino
@@ -16,7 +16,7 @@ unsigned long lastTime = 0;
 unsigned long timerDelay = 15000;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
@@ -35,41 +35,44 @@ void setup() {
 }
 
 void loop() {
+  if (Serial.available()) {
+    int x = Serial.read();
   
-  //Send an HTTP POST request every 10 seconds
-  if ((millis() - lastTime) > timerDelay) {
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-   
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
+    // Send an HTTP POST request
+    if ((millis() - lastTime) > timerDelay) {
+      //Check WiFi connection status
+      if(WiFi.status()== WL_CONNECTED){
+        WiFiClient client;
+        HTTPClient http;
      
-      // Specify content-type header
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Data to send with HTTP POST
-      String httpRequestData = "api_key=" + apiKey + "&field1=" + String(random(40));          
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);
-     
-      /*
-      // If you need an HTTP request with a content type: application/json, use the following:
-      http.addHeader("Content-Type", "application/json");
-      // JSON data to send with HTTP POST
-      String httpRequestData = "{\"api_key\":\"" + apiKey + "\",\"field1\":\"" + String(random(40)) + "\"}";          
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);*/
-     
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+        // Your Domain name with URL path or IP address with path
+        http.begin(client, serverName);
        
-      // Free resources
-      http.end();
+        // Specify content-type header
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        // Data to send with HTTP POST
+        String httpRequestData = "api_key=" + apiKey + "&field1=" + String(x);          
+        // Send HTTP POST request
+        int httpResponseCode = http.POST(httpRequestData);
+       
+        /*
+        // If you need an HTTP request with a content type: application/json, use the following:
+        http.addHeader("Content-Type", "application/json");
+        // JSON data to send with HTTP POST
+        String httpRequestData = "{\"api_key\":\"" + apiKey + "\",\"field1\":\"" + String(random(40)) + "\"}";          
+        // Send HTTP POST request
+        int httpResponseCode = http.POST(httpRequestData);*/
+       
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+         
+        // Free resources
+        http.end();
+      }
+      else {
+        Serial.println("WiFi Disconnected");
+      }
+      lastTime = millis();
     }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
   }
 }
